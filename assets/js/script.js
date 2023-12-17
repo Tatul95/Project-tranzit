74d7331cd8fd25a909c1520dafa01dbbdb567d23
Tatul95 <tatul.matevosyan.1995@gmail.com>
2023-12-17 15:09:57 +0400

    add-slide

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let reviewsSwiper = new Swiper(".picture-slider", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    loop: true,
+    navigation: {
+        nextEl: ".example-button-next",
+        prevEl: ".example-button-prev",
+    },
+
+});
